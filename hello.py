07d551a2e8d4a1c032a1c3516179f7b9688dad24
kirubhakaran s <waterkiruba@gmail.com>
2024-11-18 21:36:57 +0530

    kove

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-import pandas as pd
-print("hello wordld")
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+# Load dataset
+data = load_iris()
+X = data.data  # Features
+y = data.target  # Labels (Flower species)
+print(y)
